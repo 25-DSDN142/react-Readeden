8ec80d20e6b19747f6a96f8faeb598deafa0e323
Readeden <edenread18@gmail.com>
2025-09-30 11:56:40 +1300

    I made shapes on the face that react to the colour on the face and imput it onto the shape.

--- 2Draw_faces.js
+++ 2Draw_faces.js
@@ -61,22 +61,126 @@ function drawInteraction(faces, hands) {
 
     let noseTipX = face.keypoints[4].x;
     let noseTipY = face.keypoints[4].y;
+    
+    let Lowerforeheadx = face.keypoints[9].x;   //topforhead triangle
+    let Lowerforeheady = face.keypoints[9].y;   //topforhead triangle
+
+    let Leftforeheadx = face.keypoints[67].x;   //topforhead triangle
+    let Leftforeheady = face.keypoints[67].y;   //topforhead triangle
+
+    let Rightforeheadx = face.keypoints[297].x;   //topforhead triangle
+    let Rightforeheady = face.keypoints[297].y;   //topforhead triangle
+    
+    let middleforeheadx = face.keypoints[151].x    //topforhead triangle colour
+    let middleforeheady = face.keypoints[151].y    //topforhead triangle colour
+    
+   //Right(R) eyebrow(EB) quad(Q)
+    let RtopEBQx = face.keypoints[336].x  //right eyebrow quad
+    let RtopEBQy = face.keypoints[336].y  //right eyebrow quad
+
+    let RbottemEBQx = face.keypoints[285].x      //right eyebrow quad
+    let RbottemEBQy = face.keypoints[285].y  //right eyebrow quad
+
+    let RtopmidEBQx = face.keypoints[334].x  //right eyebrow quad
+    let RtopmidEBQy = face.keypoints[334].y  //right eyebrow quad
+
+    let RbottemmidEBQx = face.keypoints[282].x  //right eyebrow quad
+    let RbottemmidEBQy = face.keypoints[282].y  //right eyebrow quad
+
+    let RcolourEBQx = face.keypoints[295].x  //right eyebrow quad color
+    let RcolourEBQy = face.keypoints[295].y  //right eyebrow quad color
+    
+//Left(L) eyebrow(EB) quad(Q)
+    let LtopEBQx = face.keypoints[107].x  //right eyebrow quad
+    let LtopEBQy = face.keypoints[107].y  //right eyebrow quad
+
+    let LbottemEBQx = face.keypoints[55].x      //right eyebrow quad
+    let LbottemEBQy = face.keypoints[55].y  //right eyebrow quad
+
+    let LtopmidEBQx = face.keypoints[105].x  //right eyebrow quad
+    let LtopmidEBQy = face.keypoints[105].y  //right eyebrow quad
+
+    let LbottemmidEBQx = face.keypoints[52].x  //right eyebrow quad
+    let LbottemmidEBQy = face.keypoints[52].y  //right eyebrow quad
+
+    let LcolourEBQx = face.keypoints[65].x  //right eyebrow quad color
+    let LcolourEBQy = face.keypoints[65].y  //right eyebrow quad color
+
+       //Right(R) eyebrow(EB) trinagle(T)
+    let RtopEBTx = face.keypoints[334].x; 
+    let RtopEBTy = face.keypoints[334].y; 
+   
+    let RbottemEBTx = face.keypoints[282].x; 
+    let RbottemEBTy = face.keypoints[282].y; 
+
+    let RrightEBTx = face.keypoints[276].x;  
+    let RrightEBTy = face.keypoints[276].y;  
+  
+    let RcolourEBTx = face.keypoints[283].x;  
+    let RcolourEBTy = face.keypoints[283].y;   
+   
+        //Left(L) eyebrow(EB) trinagle(T)
+    let LtopEBTx = face.keypoints[105].x; 
+    let LtopEBTy = face.keypoints[105].y; 
+   
+    let LbottemEBTx = face.keypoints[52].x; 
+    let LbottemEBTy = face.keypoints[52].y; 
+
+    let LrightEBTx = face.keypoints[70].x;  
+    let LrightEBTy = face.keypoints[70].y;  
+  
+    let LcolourEBTx = face.keypoints[53].x;  
+    let LcolourEBTy = face.keypoints[53].y;   
     /*
     Start drawing on the face here
     */
-    stroke(1)
-    fill(225, 225, 0);
-    // fill(get(leftEyeCenterX, leftEyeCenterY))  //left eye fill
+  noStroke();
 
-    //ellipse(leftEyeCenterX, leftEyeCenterY, leftEyeWidth, leftEyeHeight); //Left eye fill
+//Left(L) eyebrow(EB) quad(Q)
+   let LcolourEBQ = get(LcolourEBQx,LcolourEBQy)
+   fill(LcolourEBQ);
+   quad(LtopEBQx, LtopEBQy, LbottemEBQx, LbottemEBQy, LbottemmidEBQx, LbottemmidEBQy, LtopmidEBQx, LtopmidEBQy);
 
-    drawPoints(face.leftEye);
-    drawPoints(face.leftEyebrow);
-    drawPoints(face.lips);
-    drawPoints(face.rightEye);
-    drawPoints(face.rightEyebrow);
+    //Left(L) eyebrow(EB) trinagle(T)
+   let LEBT = get(LcolourEBTx,LcolourEBTy)
+   fill(LEBT)
+   triangle(LtopEBTx,LtopEBTy,LbottemEBTx,LbottemEBTy,LrightEBTx,LrightEBTy)
 
-    // drawX(rightEyeCenterX,rightEyeCenterY);
+
+   //Right(R) eyebrow(EB) trinagle(T)
+   let REBT = get(RcolourEBTx,RcolourEBTy)
+   fill(REBT)
+   triangle(RtopEBTx,RtopEBTy,RbottemEBTx,RbottemEBTy,RrightEBTx,RrightEBTy)
+   
+ //Right(R) eyebrow(EB) quad(Q)
+   let RcolourEBQ = get(RcolourEBQx,RcolourEBQy)
+   fill(RcolourEBQ);
+   quad(RtopEBQx, RtopEBQy, RbottemEBQx, RbottemEBQy, RbottemmidEBQx, RbottemmidEBQy, RtopmidEBQx, RtopmidEBQy);
+
+    //topforhead triangle
+   let topforheadcolor = get(middleforeheadx,middleforeheady)    //topforhead triangle colour
+   fill(topforheadcolor);
+   triangle(Lowerforeheadx, Lowerforeheady, Leftforeheadx, Leftforeheady, Rightforeheadx, Rightforeheady);
+   
+   
+   
+   
+    fill(get(leftEyeCenterX, leftEyeCenterY))  //left eye fill
+    
+    ellipse(leftEyeCenterX, leftEyeCenterY, leftEyeWidth, leftEyeHeight); //Left eye fill
+    
+
+    
+    
+    
+    
+    // drawPoints(face.leftEye);
+    // drawPoints(face.leftEyebrow);
+    // drawPoints(face.lips);
+    // drawPoints(face.rightEye);
+    // drawPoints(face.rightEyebrow);
+
+    // drawX(rightEyeCenterX,rightEyeCenterY);  //crossed out eyes
     // drawX(leftEyeCenterX,leftEyeCenterY);
 
 
